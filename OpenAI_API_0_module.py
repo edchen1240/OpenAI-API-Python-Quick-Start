1ade2bb741f0c8b28eda6bc5697fd4ef8fa42f83
edchen1240 <106031570+edchen1240@users.noreply.github.com>
2024-01-09 11:49:14 -0500

    Update OpenAI_API_0_module.py

--- OpenAI_API_0_module.py
+++ OpenAI_API_0_module.py
@@ -10,11 +10,9 @@ Reference:
 Status: Complete.
 """
 
-import datetime
-import sys, os, base64
-import json, requests
+import sys, os, base64, tkinter, datetime, json, requests
+from tkinter import filedialog
 from openai import OpenAI
-import base64
 
 #[1] Basic functions.
 
@@ -27,14 +25,25 @@ def datetime_string():
     current_datetime = current_date + '_' + current_time
     return current_datetime
 
-def tkinter_select_file(dialog_title = 'Open'):
-    import tkinter as tk
-    from tkinter import filedialog
-    root = tk.Tk()
-    root.withdraw()
-    file_path = filedialog.askopenfilename(title = dialog_title)
-    print('file_path:', file_path)
-    return file_path
+def tkinter_select_file(dialog_title='Select a file'):
+    print('\n[tkinter_select_file]')
+    root = tkinter.Tk() # Creat a window (or container) for tkinter GUI (Graphical User Interface) toolkit
+    root.withdraw() # Hide the main window since we don't need to see it.
+    try:
+        file_path = filedialog.askopenfilename(title=dialog_title)
+        if file_path:
+            file_basename = os.path.basename(file_path)
+            file_path = os.path.normpath(file_path) # Clean the path
+            print('--file_path:', file_path)
+            return file_path, file_basename
+        else:
+            print('--No file selected. Exit code.')
+            sys.exit()
+    except Exception as e:
+        print(f'--An error occurred: {e}')
+        return None, None
+    finally:
+        root.destroy() # Release resources.
 
 def encode_image(image_path):
     import os
@@ -57,16 +66,17 @@ def encode_image(image_path):
 
 #[2-1] Read question from .txt file.
 def read_text_from_txt(path_txt, char_preview):
+    print('\n[read_text_from_txt]')
     try:
         with open(path_txt, 'r') as file:
             content = file.read()
-            print('[read_txt] content quick view:\n', content[:char_preview])
+            print('--Start content quick view:\n', content[:char_preview], '\n--End content quick view.')
         return content
     except FileNotFoundError:
-        print(f'[read_txt] File not found:\n {path_txt}')
+        print(f'File not found:\n {path_txt}')
         sys.exit()
     except Exception as e:
-        print(f'[read_txt] An error occurred while reading the file:\n {str(e)}')
+        print(f'An error occurred while reading the file:\n {str(e)}')
         sys.exit()
 
 #[2-2] Append reply to .txt file and save.
@@ -93,8 +103,17 @@ def append_text_to_txt(path_txt, text_to_append, char_preview): # This code will
 
 #[3] API functions
 
+def check_API_key(openai_api_key):
+    if openai_api_key == None or openai_api_key == 'put-your-api-key-here':
+        print('--API key not provided. Please put your API key at "put-your-api-key-here" in the code. Exit program.')
+        sys.exit(1)
+    return
+
+
 #[3-1] Ask text question. Used most frequent.
 def OpenAI_GPT_API_short(model_name, openai_api_key, question_content, temperature):
+    print('\n[OpenAI_GPT_API_short]')
+    check_API_key(openai_api_key)
     openai_api_url = "https://api.openai.com/v1/chat/completions"
     headers = {
         "Content-Type": "application/json",
@@ -105,6 +124,8 @@ def OpenAI_GPT_API_short(model_name, openai_api_key, question_content, temperatu
         "messages": [{'role': 'user', 'content': question_content}],
         "temperature": temperature 
     }
+    #[] Send the POST request to the OpenAI API
+    print('\n-- Sending API request. Please wait for HTTP request-response cycle.\n')
     response = requests.post(openai_api_url, headers=headers, data=json.dumps(data))
     content = None
     if response.status_code == 200:
@@ -114,8 +135,13 @@ def OpenAI_GPT_API_short(model_name, openai_api_key, question_content, temperatu
         print("Error:", response.status_code, response.text, sep='\n')
     return content
 
+
+
+
 #[3-2] Ask text question. Use this if you prefer to include additional_info such as timestamp and token usage.
 def OpenAI_GPT_API_long(model_callsigns, openai_api_key, question_content, temperature, additional_info=0):
+    print('\n[OpenAI_GPT_API_long]')
+    check_API_key(openai_api_key)
     # API portle and header for autherization.
     openai_api_url = "https://api.openai.com/v1/chat/completions"
     headers = {
@@ -131,6 +157,7 @@ def OpenAI_GPT_API_long(model_callsigns, openai_api_key, question_content, tempe
         #"temperature" is the randomness of the response. 0 is very deterministic and 1 is very creative.
     }
     #[] Send the POST request to the OpenAI API
+    print('\n--Sending API request. Please wait for HTTP request-response cycle.\n')    
     response = requests.post(openai_api_url, headers=headers, data=json.dumps(data))
     if response.status_code == 200:
         #[Dictionary] response_data were stored in a format of python dictionary with multiple "key:value" pairs.
@@ -156,6 +183,8 @@ def OpenAI_GPT_API_long(model_callsigns, openai_api_key, question_content, tempe
 
 #[3-3] Ask text and image question.
 def OpenAI_GPT_API_image_path(model_callsigns, openai_api_key, question_content, path_image, additional_info):
+    print('\n[OpenAI_GPT_API_image_path]')
+    check_API_key(openai_api_key)
     base64_image = encode_image(path_image)
     headers = {
         "Content-Type": "application/json",
@@ -177,7 +206,8 @@ def OpenAI_GPT_API_image_path(model_callsigns, openai_api_key, question_content,
         ],
         "max_tokens": 500
     }
-
+    #[] Send the POST request to the OpenAI API
+    print('\n-- Sending API request. Please wait for HTTP request-response cycle.\n')
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
     if response.status_code == 200:
         response_data = response.json()
